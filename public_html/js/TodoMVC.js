710143c861069d3963a1a18a388e3f0f3e9b5097
mikecasey <mikecasey@192.168.69.112>
2013-05-06 20:36:13 -0400

    Changed TodoMVS name to TodoMCVC. Added additional methods to TodoMVC Layout module

---
+++ public_html/js/TodoMVC.js
@@ -0,0 +1,13 @@
+var TodoMVC = new Marionette.Application();
+
+TodoMVC.addregions({
+    header :'#header',
+    main   : "#main",
+    footer : '#footer'
+});
+
+TodoMVC.on('initialize:after', function() {
+    Backbone.history.start();
+});
+
+
